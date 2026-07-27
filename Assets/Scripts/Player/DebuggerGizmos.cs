97daa7e0c89871b6ae0e1f7f2122584563d431ac
CattoElectrico <lucas8422olmedoj@gmail.com>
2026-07-27 18:19:24 -0400

    hum...this should be a commit... i know... i see... well i did somethings...

--- Assets/Scripts/Player/DebuggerGizmos.cs
+++ Assets/Scripts/Player/DebuggerGizmos.cs
@@ -1,20 +1,58 @@
+using System;
 using UnityEngine;
 
 public class DebuggerGizmos : MonoBehaviour
 {
 
-    [SerializeField] private BoxCollider2D playerCollider;
+    [SerializeField] private float radius;
+    [SerializeField] private PlayerMovement playerMovement;
+
+    [SerializeField] private BoxCollider2D boxCollider2D;
     [SerializeField] private Transform wallChecker;
     [SerializeField] private Transform groundChecker;
-    
-    void Start()
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
-        
+        DrawHitboxes();
+        DrawGroundChecker();
+        DrawWallChecker();
+    }
+
+
+    private void DrawHitboxes()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(boxCollider2D.size.x, boxCollider2D.size.y, 0));
+    }
+
+    private void DrawGroundChecker()
+    {
+        if (playerMovement.GetIsGrounded())
+        {
+            Gizmos.color = Color.green;
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+        }
+        Gizmos.DrawWireSphere(groundChecker.position, radius);
+    }
+
+    private void DrawWallChecker()
+    {
+        if (playerMovement.GetIsWalled())
+        {
+            Gizmos.color = Color.green;
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+        }
+        Gizmos.DrawWireSphere(wallChecker.position, radius);
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,168 +1,241 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Components
-    private Rigidbody2D rb;
-    private BoxCollider2D collider;
-
-    // Movement attributes
-    [SerializeField, Range(0f, 100f)] private float speed = 5f;
-    [SerializeField, Range(0f, 100f)] private float jumpForce = 10f;
-    [SerializeField] private float horizontalInput;
-    [SerializeField] private bool isFacingRight = true;
-
-    // Jump attributes
-    [SerializeField, Range(1, 10)] private int maxJumps = 2;
-    [SerializeField] private int jumpsRemaining;
-
-    // Ground check
-    [SerializeField] private LayerMask groundLayer;
-
-    // Wall interaction
-    [Header("Wall Interaction")]
+    private static readonly int JumpAnimation = Animator.StringToHash("Jump");
+    private static readonly int MovementAnimation = Animator.StringToHash("Movement");
+    private static readonly int SmoothFallAnimation = Animator.StringToHash("SmoothFall");
+    private static readonly int WallSlideAnimation = Animator.StringToHash("WallSlide");
+    private static readonly int MovementYAnimation = Animator.StringToHash("MovementY");
+    
+    [Header("Player state")]
+    [SerializeField] private PlayerStates state;
+    [Space(2)]
+    //Components
+    [SerializeField] private BoxCollider2D boxCollider2D;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
     [SerializeField] private Transform wallCheck;
-    [SerializeField] private LayerMask wallLayer;
-    [SerializeField] private float wallCheckRadius = 0.2f;
-    [SerializeField] private float wallSlidingSpeed = 2f;
-    private bool _isWallSliding;
 
-    // Falling attributes
-    [Header("Falling")]
-    [SerializeField, Range(0.5f, 5f)] private float normalGravityScale = 1f;
-    [SerializeField, Range(0.1f, 2f)] private float fallGravityMultiplier = 0.5f;
-    [SerializeField] private float smoothFallDuration = 2f;
-    [SerializeField] private Coroutine smoothFallCoroutine;
+    //Movement
+    [SerializeField] private Vector2 movement;
+    [SerializeField, Range(-1f, 1f)] private float horizontalInput;
+    [SerializeField, Range(0.1f, 20f)] private float speed = 7.7f;
+    [SerializeField, Range(0.1f, 10f)] private float jumpForce = 5;
+    [SerializeField, Range(0, 10)] private int maxJumps = 1;
+    [SerializeField] private int availableJumps;
+    [SerializeField] private bool rightMovement;
+    
+    //low fall
+    [SerializeField, Range(0.1f, 1f)] private float lowGravity = 0.5f;
+    [SerializeField] private float gravity;
+    [SerializeField, Range(0.1f, 5f)] private float timeFalling;
+    [SerializeField] private bool isLowFalling = false;
+    [SerializeField] private bool canLowFallingAgain = false;
+    
+    //Booleans states
+    [SerializeField] private bool isGrounded = false;
+    [SerializeField] private bool isWalled = false;
+    
+    //Layers
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private float radiusChecker;
+    
+    //Wall sliding attributes
+    [SerializeField] private bool isWalling;
+    [SerializeField] private bool isWallJumping = false;
+    [SerializeField, Range(0.1f, 1f)] private float wallJumpTime = 0.2f;
+    [SerializeField, Range(0.1f, 20f)] private float wallJumpForceX = 10f;
+    [SerializeField, Range(0.1f, 20f)] private float wallJumpForceY = 10f;
+    [SerializeField, Range(-1f, -0.1f)] private float smoothSliding;
+    [SerializeField, Range(1f, 10f)] private float timeSliding;
+    [SerializeField] private bool canSlidingAgain;
+    
+    //Animator
+    [SerializeField] private Animator animator;
+
 
     private void Awake()
     {
+        boxCollider2D = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
-        collider = GetComponent<BoxCollider2D>();
-        rb.gravityScale = normalGravityScale;
-        jumpsRemaining = maxJumps;
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        gravity = rb.gravityScale;
+        state = PlayerStates.Jump;
+        canSlidingAgain = false;
+        rightMovement = true;
+        horizontalInput = 0;
+        movement = new Vector2(0, 0);
+        availableJumps = maxJumps;
+        animator.SetBool(SmoothFallAnimation, false);
+        animator.SetBool(JumpAnimation, false);
+        
     }
 
     private void Update()
     {
-        if (IsGrounded() && rb.linearVelocityY <= 0.1f)
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat(MovementAnimation, Mathf.Abs(horizontalInput));
+        animator.SetFloat(MovementYAnimation, rb.linearVelocityY);
+        FlipState();
+        IsGrounded();
+        IsWalled();
+
+        if (isWalled && !isGrounded && canSlidingAgain && !isWalling)
         {
-            jumpsRemaining = maxJumps;
-            _isWallSliding = false;
+            StartCoroutine(WallSliding());
         }
 
-        HandleInput();
-        HandleJump();
-        HandleSmoothFall();
-        Flip();
+        if (Input.GetButtonDown("Jump"))
+        {
+            DoStateJump();
+        }
     }
 
     private void FixedUpdate()
     {
-        HandleMovement();
-        HandleWallSliding();
-    }
-
-    private void HandleInput()
-    {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-    }
-
-    private void HandleMovement()
-    {
-        if (!_isWallSliding)
+        if (!isWallJumping)
         {
             rb.linearVelocity = new Vector2(horizontalInput * speed, rb.linearVelocityY);
         }
     }
 
-    private void HandleWallSliding()
+    private void DoStateJump()
     {
-        if (IsTouchingWall() && !IsGrounded() && horizontalInput != 0)
+        //Jump State
+        if (availableJumps > 0 && !isWalling)
         {
-            _isWallSliding = true;
-            rb.linearVelocity = new Vector2(rb.linearVelocityX, Mathf.Max(rb.linearVelocityY, -wallSlidingSpeed));
-            jumpsRemaining = maxJumps;
+            animator.SetBool(JumpAnimation, true);
+            animator.SetBool(SmoothFallAnimation, false);
+            animator.Play("Base Layer.jump-test", 0, 0f);
+            state = PlayerStates.Jump;
+            if (!isGrounded)
+            {
+                availableJumps--;
+            }
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, 0); 
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); 
         }
-        else
+        
+        //Low Falling state
+        if (availableJumps <= 0 && !isWalling && !isLowFalling && canLowFallingAgain && rb.linearVelocityY < 0f)
         {
-            _isWallSliding = false;
+            state = PlayerStates.LowFall;
+            StartCoroutine(LowFalling());
         }
-    }
-
-    private void HandleJump()
-    {
-        if (Input.GetButtonDown("Jump") && jumpsRemaining > 0)
+        
+        //Wall sliding state
+        if (isWalled && !isGrounded && canSlidingAgain && !isWalling)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocityX, 0f);
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            jumpsRemaining--;
-        }
-    }
-
-    private void HandleSmoothFall()
-    {
-        if (Input.GetButtonDown("Jump") && IsFalling() && smoothFallCoroutine == null && jumpsRemaining == 0)
-        {
-            smoothFallCoroutine = StartCoroutine(SmoothFallRoutine());
-        }
-
-        if (IsGrounded() && smoothFallCoroutine != null)
-        {
-            StopCoroutine(smoothFallCoroutine);
-            smoothFallCoroutine = null;
-            ResetGravity();
+            StartCoroutine(WallSliding());
         }
     }
 
-    private bool IsGrounded()
+    private void FlipState()
     {
-        float extraHeight = 0.1f;
-        RaycastHit2D raycastHit = Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, extraHeight, groundLayer);
-        return raycastHit.collider != null;
-    }
-
-    private bool IsTouchingWall()
-    {
-        return Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, wallLayer);
-    }
-
-    private bool IsFalling()
-    {
-        return rb.linearVelocityY < -0.1f;
-    }
-
-    private void Flip()
-    {
-        if ((isFacingRight && horizontalInput < 0f) || (!isFacingRight && horizontalInput > 0f))
+        if (!isWallJumping && ((rb.linearVelocityX < 0f && rightMovement) || (rb.linearVelocityX > 0f && !rightMovement)))
         {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
+            rightMovement = !rightMovement;
+            transform.localScale =
+                new Vector3(transform.localScale.x * -1f, transform.localScale.y, transform.localScale.z);
         }
     }
 
-    private IEnumerator SmoothFallRoutine()
+    private IEnumerator WallSliding()
     {
-        rb.gravityScale = normalGravityScale * fallGravityMultiplier;
-        yield return new WaitForSeconds(smoothFallDuration);
-        ResetGravity();
-        smoothFallCoroutine = null;
-    }
-
-    private void ResetGravity()
-    {
-        rb.gravityScale = normalGravityScale;
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (wallCheck != null)
+        isWalling = true;
+        float timer = 0;
+        animator.SetBool(WallSlideAnimation, true);
+        while (timer < timeSliding && isWalled && !isGrounded)
         {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(wallCheck.position, wallCheckRadius);
-            //
+            if (Input.GetButtonDown("Jump"))
+            {
+                animator.Play("Base Layer.jump-test", 0, 0f);
+                isWallJumping = true;
+                rb.linearVelocity = Vector2.zero;
+                
+                float forceX = -transform.localScale.x * wallJumpForceX;
+                rb.AddForce(new Vector2(forceX, wallJumpForceY), ForceMode2D.Impulse);
+                
+                Invoke(nameof(StopWallJumping), wallJumpTime);
+                break;
+            }
+
+            timer += Time.deltaTime;
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, smoothSliding);
+            yield return null;
+        }
+        animator.SetBool(WallSlideAnimation, false);
+        canSlidingAgain = false;
+        isWalling = false;
+    }
+    
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+
+    private IEnumerator LowFalling()
+    {
+        isLowFalling = true;
+        float timer = 0;
+        rb.gravityScale = lowGravity;
+        animator.SetBool(SmoothFallAnimation, true);
+        while (timer < timeFalling && !isGrounded)
+        {
+            Debug.Log((timer < timeFalling) + " Falling");
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        canLowFallingAgain = false;
+        isLowFalling = false;
+        animator.SetBool(SmoothFallAnimation, false);
+        rb.gravityScale = gravity;
+    }
+
+    private void IsGrounded()
+    {
+        bool wasGrounded = isGrounded;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, radiusChecker, groundLayer) && rb.linearVelocityY <= 0.01f;
+
+        if (/*!wasGrounded && */isGrounded)
+        {
+            animator.SetBool(JumpAnimation, false);
+            isWallJumping = false;
+            canLowFallingAgain = true;
+            availableJumps = maxJumps;
+            canSlidingAgain = true;
         }
     }
+
+    private void IsWalled()
+    {
+        isWalled = Physics2D.OverlapCircle(wallCheck.position, radiusChecker, wallLayer);
+    }
+
+    public bool GetIsGrounded()
+    {
+        return isGrounded;
+    }
+
+
+
+    public bool GetIsWalled()
+    {
+        return isWalled;
+    }
+}
+
+public enum PlayerStates{
+    Ground,
+    Jump,
+    LowFall,
+    WallSliding
 }
